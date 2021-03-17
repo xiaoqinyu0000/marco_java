bb16052ca3a86196badad404d4d7c316a71911f6
amosyo <amosyo1994@gmail.com>
2021-03-17 20:24:09 +0800

    [feature]: add user config init from microprofile-config.properties

--- user_platform_java/serv_user/src/main/java/com/amosyo/serv/user/web/runner/ConfigRunner.java
+++ user_platform_java/serv_user/src/main/java/com/amosyo/serv/user/web/runner/ConfigRunner.java
@@ -1,9 +1,11 @@
 package com.amosyo.serv.user.web.runner;
 
+import com.amosyo.serv.user.configuration.UserConfig;
 import org.eclipse.microprofile.config.Config;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,8 +20,19 @@ public class ConfigRunner {
     @Resource(name = "bean/config")
     private Config config;
 
+    @Resource(name = "config/UserConfig")
+    private UserConfig userConfig;
+
     @PostConstruct
     public void init() {
         logger.log(Level.INFO, "Current Application Name is " + config.getValue("application.name", String.class));
+        this.initUserConfig();
+    }
+
+    private void initUserConfig() {
+        Optional.ofNullable(this.config.getValue("user.default.nick", String.class)).ifPresent(this.userConfig::setUserDefaultNick);
+        Optional.ofNullable(this.config.getValue("user.default.avatar", String.class)).ifPresent(this.userConfig::setUserDefaultAvatar);
+        Optional.ofNullable(this.config.getValue("user.default.gender", String.class)).ifPresent(this.userConfig::setUserDefaultGender);
+        Optional.ofNullable(this.config.getValue("user.default.id_step", Integer.class)).ifPresent(this.userConfig::setUserIdStep);
     }
 }
